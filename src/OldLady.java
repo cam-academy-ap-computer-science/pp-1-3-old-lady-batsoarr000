a465822aa841af7d74da7a1f1170e677450634fe
Arrow Batson <Arrow Batson@LAPTOP-3FEJDBF1>
2019-09-14 21:43:32 -0700

    Fin.

--- src/OldLady.java
+++ src/OldLady.java
@@ -2,7 +2,12 @@
 public class OldLady {
 
 	public static void main(String[] args) {
-		System.out.println("OldLady");
+		System.out.println(" ______            ______                                 ______                    ");
+		System.out.println("|      |  |        |     \\          |             /\\      |     \\     \\   /             ");
+		System.out.println("|      |  |        |      \\         |            /  \\     |      \\     \\ /                           ");
+		System.out.println("|      |  |        |       |        |           /____\\    |       |     |                 ");
+		System.out.println("|      |  |        |       /        |          /      \\   |       /     |         ");
+		System.out.println("|______|  |_______ |_____ /         |_______  /        \\  |______/      |                ");
 
 	}
 
