f9a3e57d0c80e9fc0de6dd1b18067f916828ae8f
batsoarr000 <batsoarr000@CAM-LAB-10.bgsd.k12.wa.us>
2019-09-16 07:33:43 -0700

    Done

--- src/OldLady.java
+++ src/OldLady.java
@@ -2,12 +2,42 @@
 public class OldLady {
 
 	public static void main(String[] args) {
-		System.out.println(" ______            ______                                 ______                    ");
-		System.out.println("|      |  |        |     \\          |             /\\      |     \\     \\   /             ");
-		System.out.println("|      |  |        |      \\         |            /  \\     |      \\     \\ /                           ");
-		System.out.println("|      |  |        |       |        |           /____\\    |       |     |                 ");
-		System.out.println("|      |  |        |       /        |          /      \\   |       /     |         ");
-		System.out.println("|______|  |_______ |_____ /         |_______  /        \\  |______/      |                ");
+		System.out.println("There was am old lady who swallowed a fly.");
+		method1();
+		System.out.println("There was an old lady who swallowed a spider,");
+		System.out.println("That wriggled and iggled and jiggled inside her.");
+		method2();
+		System.out.println("There was an old lady who swallowed a bird,");
+		System.out.println("How absurd to swallow a bird.");
+		method3();
+		System.out.println("There was an old lady who swallowed a cat,");
+		System.out.println("Imagine that to  swallow a cat.");
+		method4();
+		System.out.println("There was an old lady who swallowed a dog,");
+		System.out.println("What a hog to swallow a dog.");
+		method5();
+		System.out.println("There was an old lady who sallowed a horse,");
+		System.out.println("She died of course.");
+	}
+	public static void method1() {
+		System.out.println("I don't know why she swallowed that fly,");
+		System.out.println("Perhapse she'll die.");
+	}
+	public static void method2() {
+		System.out.println("She swallowed the spider to chatch the fly,");
+		method1();
+	}
+	public static void method3() {
+		System.out.println("She swallowed the bird to chatch the spider,");
+		method2();
+	}
+	public static void method4() {
+		System.out.println("She swallowed the cat to chatch the bird");
+		method3();
+	}
+	public static void method5() {
+		System.out.println("She swallowed the dog to chatch the cat");
+		method4();
 
 	}
 
