d7170bf4e820e5fe9422841f3777b5d86054ff5d
batsoarr000 <batsoarr000@CAM-LAB-10.bgsd.k12.wa.us>
2019-09-17 07:15:45 -0700

    Change

--- src/OldLady.java
+++ src/OldLady.java
@@ -3,41 +3,41 @@ public class OldLady {
 
 	public static void main(String[] args) {
 		System.out.println("There was am old lady who swallowed a fly.");
-		method1();
+		fly();
 		System.out.println("There was an old lady who swallowed a spider,");
 		System.out.println("That wriggled and iggled and jiggled inside her.");
-		method2();
+		spider();
 		System.out.println("There was an old lady who swallowed a bird,");
 		System.out.println("How absurd to swallow a bird.");
-		method3();
+		bird();
 		System.out.println("There was an old lady who swallowed a cat,");
 		System.out.println("Imagine that to  swallow a cat.");
-		method4();
+		cat();
 		System.out.println("There was an old lady who swallowed a dog,");
 		System.out.println("What a hog to swallow a dog.");
-		method5();
+		dog();
 		System.out.println("There was an old lady who sallowed a horse,");
 		System.out.println("She died of course.");
 	}
-	public static void method1() {
+	public static void fly() {
 		System.out.println("I don't know why she swallowed that fly,");
 		System.out.println("Perhapse she'll die.");
 	}
-	public static void method2() {
+	public static void spider() {
 		System.out.println("She swallowed the spider to chatch the fly,");
-		method1();
+		fly();
 	}
-	public static void method3() {
+	public static void bird() {
 		System.out.println("She swallowed the bird to chatch the spider,");
-		method2();
+		spider();
 	}
-	public static void method4() {
+	public static void cat() {
 		System.out.println("She swallowed the cat to chatch the bird");
-		method3();
+		bird();
 	}
-	public static void method5() {
+	public static void dog() {
 		System.out.println("She swallowed the dog to chatch the cat");
-		method4();
+		cat();
 
 	}
 
